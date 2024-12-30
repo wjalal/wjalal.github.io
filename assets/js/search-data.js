50c76d8fd7f753b3caedabc9778c2e2a3b67e6d5
wjalal <jalalwasif@gmail.com>
2024-12-30 09:17:49 +0000

    Deploying to gh-pages from @ wjalal/wjalal.github.io@4801ec963e388141400cd774ac385677d11e07f7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -393,22 +393,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-busbuddy",
-          title: 'BusBuddy',
-          description: "A mobile app for universities, enabling ticket purchases, bus tracking, schedules, and predictive algorithms.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
+          section: "News",},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
@@ -423,21 +408,26 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-busbuddy",
+          title: 'BusBuddy',
+          description: "A mobile app for universities, enabling ticket purchases, bus tracking, schedules, and predictive algorithms.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/busbuddy/";
+            },},{id: "projects-emergencychat",
+          title: 'EmergencyChat',
+          description: "Simple cross-platform chat and file sharing web app that kept us connected during a crisis",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/emchat/";
+            },},{id: "projects-railbuddy",
+          title: 'RailBuddy',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/railbuddy/";
             },},{
         id: 'social-discord',
         title: 'Discord',
