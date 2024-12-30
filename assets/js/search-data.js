7cad171fe5daea7c5aeb979c66f2b228fbc8541c
wjalal <jalalwasif@gmail.com>
2024-12-30 08:29:12 +0000

    Deploying to gh-pages from @ wjalal/wjalal.github.io@c434f5d82fe0e71363ab3692db1c8316fcb404f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -393,9 +393,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-busbuddy",
+          title: 'BusBuddy',
+          description: "A mobile app for universities, enabling ticket purchases, bus tracking, schedules, and predictive algorithms.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
